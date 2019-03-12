2b11bedc78f7850044d5141e469bd86619eeb44b
Zhang Han labPC <455209990@qq.com>
2019-03-12 16:27:41 +0800

    圆弧轨迹加方向两遍

--- googol/main.cpp
+++ googol/main.cpp
@@ -12,221 +12,222 @@ int main(int argc, char *argv[])    //正逆街测试
 {
     QCoreApplication e(argc, argv);
 
-//    vector<vector<double>> pos;
-//    QFile file("C:/Users/df/Desktop/Googol_point_move/googol/test.txt");
-//    if(!file.open(QIODevice::ReadOnly | QIODevice::Text))
-//    {
-//        cout<<"Can't open the file!"<<endl;
-//    }
+    vector<vector<double>> pos;
+    QFile file("C:/Users/df/Desktop/Googol_point_move/googol/test.txt");
+    if(!file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        cout<<"Can't open the file!"<<endl;
+    }
 
-//    QStringList list;
-//    while(!file.atEnd())
-//    {
-//        QByteArray line = file.readLine();
-//        QString str(line);
-//        list = str.split("\t");
+    QStringList list;
+    while(!file.atEnd())
+    {
+        QByteArray line = file.readLine();
+        QString str(line);
+        list = str.split("\t");
 
-//        double x = list[0].toDouble();
-//        double y = list[1].toDouble();
-//        double z = list[2].toDouble();
+        double x = list[0].toDouble();
+        double y = list[1].toDouble();
+        double z = list[2].toDouble();
 
-//        vector<double> temp;
-//        temp.push_back(x);
-//        temp.push_back(y);
-//        temp.push_back(z);
-//        pos.push_back(temp);
-//    }
+        vector<double> temp;
+        temp.push_back(x);
+        temp.push_back(y);
+        temp.push_back(z);
+        pos.push_back(temp);
+    }
 
+    vector<vector<double>> pos1;
+    QFile file1("C:/Users/df/Desktop/Googol_point_move/googol/luoxuanxian.txt");
+    if(!file1.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        cout<<"Can't open the file!"<<endl;
+    }
 
-//    vector<long> time;
-//    QFile file1("C:/Users/df/Desktop/Googol_point_move/googol/time.txt");
-//    if(!file1.open(QIODevice::ReadOnly | QIODevice::Text))
-//    {
-//        cout<<"Can't open the file!"<<endl;
-//    }
+    QStringList list1;
+    while(!file1.atEnd())
+    {
+        QByteArray line1 = file1.readLine();
+        QString str(line1);
+        list1 = str.split("\t");
 
-//    while(!file1.atEnd())
-//    {
-//        QByteArray line = file1.readLine();
-//        QString str(line);
-//        time.push_back(str.toLong());
-//    }
+        double x = list1[0].toDouble();
+        double y = list1[1].toDouble();
+        double z = list1[2].toDouble();
 
-//    cout<<time.size()<<endl;
-//    cout<<pos.size()<<endl;
+        vector<double> temp;
+        temp.push_back(x);
+        temp.push_back(y);
+        temp.push_back(z);
+        pos1.push_back(temp);
+    }
 
+    cout<<pos.size()<<endl;
+    cout<<pos1.size()<<endl;
 
-//    cout.setf(ios::fixed);
-//    cout.precision(16);
-//    short flag=1;
-//    long lGpiValue;
+    cout.setf(ios::fixed);
+    cout.precision(16);
+    short flag=1;
+    long lGpiValue;
 
-//    AssemblyRobot myrobot;
-//    cout << "myrobot.Initial():"<<myrobot.Initial(RbtAllAxis)<<endl;
+    AssemblyRobot myrobot;
+    cout << "myrobot.Initial():"<<myrobot.Initial(RbtAllAxis)<<endl;
 
-//    vector<long> time1;
-//    for(int i=0;i<pos.size();i++)
-//    {
-//        time1.push_back(8*i);
-//    }
-//    for(int i =0;i<pos.size();i++)
-//    {
-//        cout<<time1[i]<<endl;
-//    }
+    vector<long> time1;
+    for(int i=0;i<pos.size();i++)
+    {
+        time1.push_back(2*i);
+    }
 
-//    vector<long> time2;
-//    for(int i=0;i<pos.size();i++)
-//    {
-//        time2.push_back(4*i);
-//    }
-//    for(int i =0;i<pos.size();i++)
-//    {
-//        cout<<time2[i]<<endl;
-//    }
+    vector<long> time2;
+    for(int i=0;i<pos1.size();i++)
+    {
+        time2.push_back(8*i);
+    }
 
-//    while(flag == 1)
-//    {
-//        GT_GetDi(MC_GPI,&lGpiValue);
-//        if(lGpiValue& (1<<1))
-//        {
-//            if(myrobot.PointMove(pos,time1,pos.size())!=RobotSuccess)
-//                cout<<"oh my god"<<endl;
-
-//            if(myrobot.PointMove(pos,time2,pos.size())!=RobotSuccess)
-//                cout<<"oh my god"<<endl;
-//        }
-//        if(lGpiValue& (1<<powerIO))
-//        {
-//            flag = 0;
-//        }
-//    }
-//    cout <<"myrobot.Close():"<< myrobot.Close()<<endl;
-
-
-
-        cout.setf(ios::fixed);
-        cout.precision(16);
-        short flag=1;
-        long lGpiValue;
-
-        AssemblyRobot myrobot;
-        cout << "myrobot.Initial():"<<myrobot.Initial(RbtAllAxis)<<endl;
-
-        double vel1[1][3] = {{0.005,0.3,0.005}};
-        double vel2[1][3] = {{0.01,0.15,0.01}};
-        double vel3[1][3] = {{0.01,0.1,0.01}};
-
-        double ang0[2][3] = {{0,0,0},{0,0,0}};
-        double ang1[2][3] = {{0,0,0},{-20,0,0}};
-        double ang2[2][3] = {{0,0,0},{20,0,0}};
-        double ang3[2][3] = {{0,0,0},{0,-20,0}};
-        double ang4[2][3] = {{0,0,0},{0,20,0}};
-        double ang5[2][3] = {{0,0,0},{0,0,-20}};
-        double ang6[2][3] = {{0,0,0},{0,0,20}};
-
-        double ang7[2][3] = {{0,0,0},{-10,10,0}};
-        double ang8[2][3] = {{0,0,0},{-10,-10,0}};
-        double ang9[2][3] = {{0,0,0},{10,-10,0}};
-        double ang10[2][3] = {{0,0,0},{10,10,0}};
-
-        while(flag == 1)
+    while(flag == 1)
+    {
+        GT_GetDi(MC_GPI,&lGpiValue);
+        if(lGpiValue& (1<<1))
         {
-            GT_GetDi(MC_GPI,&lGpiValue);
-            if(lGpiValue& (1<<1))
-            {
-                for(int o=0;o<1;o++)
-                {
-                    if(myrobot.AngMove(ang5,vel3,1)!=RobotSuccess)
-                        cout<<"oh my god"<<endl;
+            if(myrobot.PointMove(pos,time1,pos.size())!=RobotSuccess)
+                cout<<"oh my god"<<endl;
 
-                    if(myrobot.AngMove(ang6,vel3,1)!=RobotSuccess)
-                        cout<<"oh my god"<<endl;
-
-                    if(myrobot.AngMove(ang0,vel3,1)!=RobotSuccess)
-                        cout<<"oh my god"<<endl;
-
-                    if(myrobot.AngMove(ang3,vel1,1)!=RobotSuccess)
-                        cout<<"oh my god"<<endl;
-
-                    if(myrobot.AngMove(ang4,vel1,1)!=RobotSuccess)
-                        cout<<"oh my god"<<endl;
-
-                    if(myrobot.AngMove(ang0,vel1,1)!=RobotSuccess)
-                        cout<<"oh my god"<<endl;
-
-                    if(myrobot.AngMove(ang1,vel1,1)!=RobotSuccess)
-                        cout<<"oh my god"<<endl;
-
-                    if(myrobot.AngMove(ang2,vel1,1)!=RobotSuccess)
-                        cout<<"oh my god"<<endl;
-
-                    if(myrobot.AngMove(ang0,vel1,1)!=RobotSuccess)
-                        cout<<"oh my god"<<endl;
-                }
-
-                for(int o=0;o<3;o++)
-                {
-                    if(myrobot.AngMove(ang4,vel1,1)!=RobotSuccess)
-                        cout<<"oh my god"<<endl;
-
-                    if(myrobot.AngMove(ang1,vel1,1)!=RobotSuccess)
-                        cout<<"oh my god"<<endl;
-
-                    if(myrobot.AngMove(ang3,vel1,1)!=RobotSuccess)
-                        cout<<"oh my god"<<endl;
-
-                    if(myrobot.AngMove(ang2,vel1,1)!=RobotSuccess)
-                        cout<<"oh my god"<<endl;
-
-                    if(myrobot.AngMove(ang0,vel3,1)!=RobotSuccess)
-                        cout<<"oh my god"<<endl;
-
-                    if(myrobot.AngMove(ang4,vel1,1)!=RobotSuccess)
-                        cout<<"oh my god"<<endl;
-                    if(myrobot.AngMove(ang7,vel2,1)!=RobotSuccess)
-                        cout<<"oh my god"<<endl;
-
-                    if(myrobot.AngMove(ang1,vel1,1)!=RobotSuccess)
-                        cout<<"oh my god"<<endl;
-                    if(myrobot.AngMove(ang8,vel2,1)!=RobotSuccess)
-                        cout<<"oh my god"<<endl;
-
-                    if(myrobot.AngMove(ang3,vel1,1)!=RobotSuccess)
-                        cout<<"oh my god"<<endl;
-                    if(myrobot.AngMove(ang9,vel2,1)!=RobotSuccess)
-                        cout<<"oh my god"<<endl;
-
-                    if(myrobot.AngMove(ang2,vel1,1)!=RobotSuccess)
-                        cout<<"oh my god"<<endl;
-                    if(myrobot.AngMove(ang10,vel2,1)!=RobotSuccess)
-                        cout<<"oh my god"<<endl;
-
-                    if(myrobot.AngMove(ang0,vel3,1)!=RobotSuccess)
-                        cout<<"oh my god"<<endl;
-
-                    if(myrobot.AngMove(ang7,vel2,1)!=RobotSuccess)
-                        cout<<"oh my god"<<endl;
-
-                    if(myrobot.AngMove(ang8,vel2,1)!=RobotSuccess)
-                        cout<<"oh my god"<<endl;
-
-                    if(myrobot.AngMove(ang9,vel2,1)!=RobotSuccess)
-                        cout<<"oh my god"<<endl;
-
-                    if(myrobot.AngMove(ang10,vel2,1)!=RobotSuccess)
-                        cout<<"oh my god"<<endl;
-
-                    if(myrobot.AngMove(ang0,vel2,1)!=RobotSuccess)
-                        cout<<"oh my god"<<endl;
-                }
-            }
-
-            if(lGpiValue& (1<<powerIO))
-            {
-                flag = 0;
-            }
+            if(myrobot.PointMove(pos1,time2,pos1.size())!=RobotSuccess)
+                cout<<"oh my god"<<endl;
         }
-        cout <<"myrobot.Close():"<< myrobot.Close()<<endl;
+        if(lGpiValue& (1<<powerIO))
+        {
+            flag = 0;
+        }
+    }
+    cout <<"myrobot.Close():"<< myrobot.Close()<<endl;
+
+
+
+//        cout.setf(ios::fixed);
+//        cout.precision(16);
+//        short flag=1;
+//        long lGpiValue;
+
+//        AssemblyRobot myrobot;
+//        cout << "myrobot.Initial():"<<myrobot.Initial(RbtAllAxis)<<endl;
+
+//        double vel1[1][3] = {{0.005,0.3,0.005}};
+//        double vel2[1][3] = {{0.01,0.2,0.01}};
+//        double vel3[1][3] = {{0.01,0.15,0.01}};
+
+//        double ang0[2][3] = {{0,0,0},{0,0,0}};
+//        double ang1[2][3] = {{0,0,0},{-25,0,0}};
+//        double ang2[2][3] = {{0,0,0},{25,0,0}};
+//        double ang3[2][3] = {{0,0,0},{0,-25,0}};
+//        double ang4[2][3] = {{0,0,0},{0,25,0}};
+//        double ang5[2][3] = {{0,0,0},{0,0,-25}};
+//        double ang6[2][3] = {{0,0,0},{0,0,25}};
+
+//        double ang7[2][3] = {{0,0,0},{-15,15,0}};
+//        double ang8[2][3] = {{0,0,0},{-15,-15,0}};
+//        double ang9[2][3] = {{0,0,0},{15,-15,0}};
+//        double ang10[2][3] = {{0,0,0},{15,15,0}};
+
+//        while(flag == 1)
+//        {
+//            GT_GetDi(MC_GPI,&lGpiValue);
+//            if(lGpiValue& (1<<1))
+//            {
+//                for(int o=0;o<2;o++)
+//                {
+//                    if(myrobot.AngMove(ang5,vel3,1)!=RobotSuccess)
+//                        cout<<"oh my god"<<endl;
+
+//                    if(myrobot.AngMove(ang6,vel3,1)!=RobotSuccess)
+//                        cout<<"oh my god"<<endl;
+
+//                    if(myrobot.AngMove(ang0,vel3,1)!=RobotSuccess)
+//                        cout<<"oh my god"<<endl;
+
+//                    if(myrobot.AngMove(ang3,vel1,1)!=RobotSuccess)
+//                        cout<<"oh my god"<<endl;
+
+//                    if(myrobot.AngMove(ang4,vel1,1)!=RobotSuccess)
+//                        cout<<"oh my god"<<endl;
+
+//                    if(myrobot.AngMove(ang0,vel1,1)!=RobotSuccess)
+//                        cout<<"oh my god"<<endl;
+
+//                    if(myrobot.AngMove(ang1,vel1,1)!=RobotSuccess)
+//                        cout<<"oh my god"<<endl;
+
+//                    if(myrobot.AngMove(ang2,vel1,1)!=RobotSuccess)
+//                        cout<<"oh my god"<<endl;
+
+//                    if(myrobot.AngMove(ang0,vel1,1)!=RobotSuccess)
+//                        cout<<"oh my god"<<endl;
+//                }
+
+//                for(int o=0;o<2;o++)
+//                {
+//                    if(myrobot.AngMove(ang4,vel1,1)!=RobotSuccess)
+//                        cout<<"oh my god"<<endl;
+
+//                    if(myrobot.AngMove(ang1,vel1,1)!=RobotSuccess)
+//                        cout<<"oh my god"<<endl;
+
+//                    if(myrobot.AngMove(ang3,vel1,1)!=RobotSuccess)
+//                        cout<<"oh my god"<<endl;
+
+//                    if(myrobot.AngMove(ang2,vel1,1)!=RobotSuccess)
+//                        cout<<"oh my god"<<endl;
+
+//                    if(myrobot.AngMove(ang0,vel3,1)!=RobotSuccess)
+//                        cout<<"oh my god"<<endl;
+
+//                    if(myrobot.AngMove(ang4,vel1,1)!=RobotSuccess)
+//                        cout<<"oh my god"<<endl;
+//                    if(myrobot.AngMove(ang7,vel2,1)!=RobotSuccess)
+//                        cout<<"oh my god"<<endl;
+
+//                    if(myrobot.AngMove(ang1,vel1,1)!=RobotSuccess)
+//                        cout<<"oh my god"<<endl;
+//                    if(myrobot.AngMove(ang8,vel2,1)!=RobotSuccess)
+//                        cout<<"oh my god"<<endl;
+
+//                    if(myrobot.AngMove(ang3,vel1,1)!=RobotSuccess)
+//                        cout<<"oh my god"<<endl;
+//                    if(myrobot.AngMove(ang9,vel2,1)!=RobotSuccess)
+//                        cout<<"oh my god"<<endl;
+
+//                    if(myrobot.AngMove(ang2,vel1,1)!=RobotSuccess)
+//                        cout<<"oh my god"<<endl;
+//                    if(myrobot.AngMove(ang10,vel2,1)!=RobotSuccess)
+//                        cout<<"oh my god"<<endl;
+
+//                    if(myrobot.AngMove(ang0,vel3,1)!=RobotSuccess)
+//                        cout<<"oh my god"<<endl;
+
+//                    if(myrobot.AngMove(ang7,vel2,1)!=RobotSuccess)
+//                        cout<<"oh my god"<<endl;
+
+//                    if(myrobot.AngMove(ang8,vel2,1)!=RobotSuccess)
+//                        cout<<"oh my god"<<endl;
+
+//                    if(myrobot.AngMove(ang9,vel2,1)!=RobotSuccess)
+//                        cout<<"oh my god"<<endl;
+
+//                    if(myrobot.AngMove(ang10,vel2,1)!=RobotSuccess)
+//                        cout<<"oh my god"<<endl;
+
+//                    if(myrobot.AngMove(ang0,vel2,1)!=RobotSuccess)
+//                        cout<<"oh my god"<<endl;
+//                }
+//            }
+
+//            if(lGpiValue& (1<<powerIO))
+//            {
+//                flag = 0;
+//            }
+//        }
+//        cout <<"myrobot.Close():"<< myrobot.Close()<<endl;
 
     return e.exec();
 }
